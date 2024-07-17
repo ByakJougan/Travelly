c16936b910e8ac3bf5bd656281006a1dd068f7c5
ByakJougan <118471153+ByakJougan@users.noreply.github.com>
2024-07-17 22:31:05 +0700

    clean code pt2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import Router from './router';
-import { UserProvider } from './pages/UserContext'; // Adjust the path accordingly
+import { UserProvider } from './pages/UserContext';
 
 export default function App() {
   return (
